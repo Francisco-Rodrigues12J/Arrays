7982464a9c6fded4451ea03df654337231ea0c40
Francisco-Rodrigues12J <113584448+Francisco-Rodrigues12J@users.noreply.github.com>
2022-10-13 08:39:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let text="";
+const cars = ["Toyota","Nissan","Mazda"];
+cars.push("Honda");
+cars.sort();
+
+
+function MostraArray(){
+cars.forEach(listArray)
+
+document.getElementById("demo").innerHTML = text;
+
+
+
+function listArray(item, index){
+
+text += index + "; " + item + "</br>";
+
+}
+}
+
+
